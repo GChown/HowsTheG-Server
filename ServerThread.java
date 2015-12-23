3d93fdf70f306d45496379b8c43ee7b9a6fde301
Gordie Chown <gordie.chown@gmail.com>
2015-12-23 00:23:00 -0500

    Add multithreading, light computation
    Will round to nearest 0.5 when calculating average..eventually.

--- ServerThread.java
+++ ServerThread.java
@@ -8,11 +8,13 @@ import java.net.SocketTimeoutException;
 
 public class ServerThread extends Thread {
 	private ServerSocket socket;
-	DataInputStream in;
-	DataOutputStream out;
-	private Socket[] clients;
-	private int manyClients;
 	private Socket server;
+	private DataInputStream in;
+	private DataOutputStream out;
+	private float rating;
+	private int numVotes;
+	private float totRating;
+
 	public static void main(String[] args){
 	try{
 		new ServerThread().start();
@@ -20,45 +22,77 @@ public class ServerThread extends Thread {
 		e.printStackTrace();
 	}
 	}
+
 	public ServerThread() throws IOException {
-		clients = new Socket[10];
-		manyClients = 0;
 		socket = new ServerSocket(8000);
 		System.out.println("Running on " + socket);
+		rating = 0;
+		numVotes = 0;
+		totRating = 0;
 	}
 
 	public void run() {
-		while (true) {
+		boolean listen = true;
+		while (listen) {
 			try {
 				server = socket.accept();
-				System.out.println("Incoming client " + manyClients + ": " + server.getRemoteSocketAddress());
-				clients[manyClients] = server;
-				manyClients++;
-				for(Socket s : clients){
-					in = new DataInputStream(server.getInputStream());
-					out = new DataOutputStream(server.getOutputStream());
-					int request = in.readInt();
-					System.out.println("Recieved request " + request);
-					if(request == 0){
-						System.out.println("Sending bytes");
-						byte[] sending = new byte[2];
-						sending[0] = 5;
-						sending[1] = 19;
-						out.write(sending);
-					}else if(request == 1){
-						System.out.println("Sending string");
-						out.writeUTF("This is the string that was sent");
-					}	
-				}
+				new Thread(new ClientThread(server)).start();
 			} catch (SocketTimeoutException k) {
 				System.err.println("Socket timed out!");
 			} catch(EOFException e){
-				System.out.println("Client " + manyClients + "disconnected");
-				manyClients--;
+				System.out.println("Client disconnected");
 				continue;
 			} catch (IOException e) {
 				e.printStackTrace();
 				break;
+			}
+		}
+	}
+	class ClientThread implements Runnable{
+		Socket socket;
+		int request;
+		ClientThread(Socket sock){
+			this.socket = sock;
+			try{
+				in = new DataInputStream(socket.getInputStream());
+				out = new DataOutputStream(socket.getOutputStream());
+				request = -1;
+			}catch(EOFException e){
+				System.out.println("Client disconnected");
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+		public void run(){
+			boolean runIt = true;
+			while(runIt){
+			try{
+				request = in.readInt();
+				//Request 0 is send Rating (incoming), request 1 is get rating and numVotes
+				System.out.println("Recieved request " + request + " from client " + socket.getInetAddress().getHostName() + "; ");
+				if(request == 0){
+					System.out.print("Incoming score: ");
+					int usrRating = in.readInt();
+					System.out.print(usrRating);
+					numVotes++;
+					totRating += usrRating;
+					rating = totRating / numVotes;
+					rating = ((float) Math.floor(rating * 10)) / 10;
+					System.out.println(", number of votes: " + numVotes + ", total rating: " + totRating
+							+ ", average rating: " + rating);
+				}else if(request == 1){
+					System.out.println("Sending score " + rating + ", numVotes " + numVotes);
+					out.writeFloat(rating);
+					out.writeInt(numVotes);
+				} else{
+				       	out.writeUTF("Bad request");	
+				}
+			}catch(EOFException e){
+				System.out.println("Client disconnected");
+				runIt = false;
+			}catch(IOException e){
+				e.printStackTrace();
+			}
 			}
 		}
 	}
