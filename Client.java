0e68ad7e7fcede097eb45422189efb548d2eaa7c
Gordie Chown <gordie.chown@gmail.com>
2015-12-25 22:25:53 -0500

    Fix Server bad request, add Client menu
    Server used to have a fatal crash if first int wasn't 0 or 1. Added a menu for the client so changing values doesn't suck.

--- Client.java
+++ Client.java
@@ -1,4 +1,5 @@
 import java.net.Socket;
+import java.util.Scanner;
 import java.io.IOException;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -6,24 +7,37 @@ public class Client{
 	static DataInputStream din;
 	static DataOutputStream dout;
 	static Socket sock;
+	static Scanner keyb;
 	public static void main(String[] args){
-		try{
-		sock = new Socket("localhost", 8000);
-		din = new DataInputStream(sock.getInputStream());
-		dout = new DataOutputStream(sock.getOutputStream());
-		dout.writeInt(1);
-		String text = din.readUTF();
-		System.out.println(text);
-		dout.writeInt(0);
-		byte[] incoming = new byte[2];
-		din.read(incoming);
-		System.out.println("Reading bytes:");
-		for(byte b : incoming){
-			System.out.println(b);
+		keyb = new Scanner(System.in);
+		int option = -1;
+		if(args.length != 0) option = Integer.parseInt(args[0]);
+		else{
+			System.out.println("--Menu--\n1. Recieve Scores\n2. Send Score");
+			option = keyb.nextInt();
 		}
+		try{
+			sock = new Socket("IPGOESHERE.com", 8000);
+			din = new DataInputStream(sock.getInputStream());
+			dout = new DataOutputStream(sock.getOutputStream());
+			if(option == 1){
+				dout.writeInt(1);
+				float rating = din.readFloat();
+				int numVotes = din.readInt();
+				System.out.println("Rating is " + rating + " (" + numVotes + ")");
+			}else if(option == 2){
+				//Send 0 then the rating
+				System.out.println("Enter score to send:");
+				int sending = keyb.nextInt();
+				dout.writeInt(0);
+				dout.writeInt(sending);
+			}else{
+				System.out.println("Should sent 1 or 2");
+			}
 		}catch(IOException e){
 			e.printStackTrace();
 		}
 
 	}
 }
+
--- ServerThread.java
+++ ServerThread.java
@@ -36,6 +36,7 @@ public class ServerThread extends Thread {
 		while (listen) {
 			try {
 				server = socket.accept();
+				System.out.println("Connection from " + server);
 				new Thread(new ClientThread(server)).start();
 			} catch (SocketTimeoutException k) {
 				System.err.println("Socket timed out!");
@@ -73,6 +74,10 @@ public class ServerThread extends Thread {
 				if(request == 0){
 					System.out.print("Incoming score: ");
 					int usrRating = in.readInt();
+					if(usrRating > 6 || usrRating < 1){
+						out.writeUTF("Error: Must be between 1-5");
+						break;
+					}
 					System.out.print(usrRating);
 					numVotes++;
 					totRating += usrRating;
@@ -84,12 +89,15 @@ public class ServerThread extends Thread {
 					System.out.println("Sending score " + rating + ", numVotes " + numVotes);
 					out.writeFloat(rating);
 					out.writeInt(numVotes);
-				} else{
-				       	out.writeUTF("Bad request");	
+				}else{
+					System.out.println("Bad request recieved");
 				}
 			}catch(EOFException e){
 				System.out.println("Client disconnected");
 				runIt = false;
+			}catch(java.net.SocketException e){
+				System.out.println("Error with client " + socket); 
+				e.printStackTrace();
 			}catch(IOException e){
 				e.printStackTrace();
 			}
